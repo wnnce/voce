90e01c0f92af8618c64c93de1921ccde8aeb499e
ranchangxin <ranchangxin@gmail.com>
2026-03-29 21:01:03 +0800

    feat: add markdown filter plugin

--- internal/plugins/init.go
+++ internal/plugins/init.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/wnnce/voce/internal/plugins/elevenlabs_tts"
 	_ "github.com/wnnce/voce/internal/plugins/google_asr"
 	_ "github.com/wnnce/voce/internal/plugins/interrupter"
+	_ "github.com/wnnce/voce/internal/plugins/md_filter"
 	_ "github.com/wnnce/voce/internal/plugins/minimax_tts"
 	_ "github.com/wnnce/voce/internal/plugins/openai_llm"
 	_ "github.com/wnnce/voce/internal/plugins/openai_tts"
